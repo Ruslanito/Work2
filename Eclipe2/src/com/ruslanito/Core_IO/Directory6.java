5d93a0b7e15147786f5c1b234d77cc518b9407d1
Ruslan <breitovo76@gmail.com>
2017-04-05 15:43:40 +0300

    changes in Directory6

--- Eclipe2/src/com/ruslanito/Core_IO/Directory6.java
+++ Eclipe2/src/com/ruslanito/Core_IO/Directory6.java
@@ -6,16 +6,14 @@ public class Directory6 {
 
 	public static void main(String[] args) {
 		// создаём файл directory6.txt с дополнением текста в нём
-		File myFile = new File("e://printwriter5.txt"); 
-		// создаём директорию MyDirectory  
-		File myDir = new File("e://MyDirectory"); 
+		File myFile = new File("e://printwriter5.txt");
+		// создаём директорию MyDirectory
+		File myDir = new File("e://MyDirectory");
 		myDir.mkdir();
-		
-		
 
 		try {
 			// используем ссылку на файл
-			PrintWriter writer = new PrintWriter (new BufferedWriter(new FileWriter(myFile, true)));
+			PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(myFile, true)));
 			// запись информационной строки
 			writer.println("Helo PrintWriter5");
 			// Очистим поток
