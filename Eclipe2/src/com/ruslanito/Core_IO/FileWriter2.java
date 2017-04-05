db9c5f9d700e76796e3b9ac72e6a88fa99e42b9a
Ruslan <breitovo76@gmail.com>
2017-04-05 15:06:55 +0300

    code in FileWriter2

--- Eclipe2/src/com/ruslanito/Core_IO/FileWriter2.java
+++ Eclipe2/src/com/ruslanito/Core_IO/FileWriter2.java
@@ -2,14 +2,17 @@ package com.ruslanito.Core_IO;
 
 import java.io.*;
 
-public class FileWriter1 {
+public class FileWriter2 {
 
 	public static void main(String[] args) {
+		// создаём файл filewriter2.txt
+		File myFile = new File("e://filewriter2.txt");
+
 		try {
-			// создаём файл filewriter.txt
-			FileWriter writer = new FileWriter("e://filewriter.txt");
+			// используем ссылку на файл
+			FileWriter writer = new FileWriter(myFile);
 			// запись информационной строки
-			writer.write("Helo FileWriter ");
+			writer.write("Helo FileWriter2 ");
 			// закрываем поток
 			writer.close();
 
