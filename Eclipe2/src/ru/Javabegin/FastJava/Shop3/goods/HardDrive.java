2bf3cc201814589250479736d699626e0d9ae4b2
Ruslan <breitovo76@gmail.com>
2017-04-08 15:23:50 +0300

    add code in HardDrive

--- Eclipe2/src/ru/Javabegin/FastJava/Shop3/goods/HardDrive.java
+++ Eclipe2/src/ru/Javabegin/FastJava/Shop3/goods/HardDrive.java
@@ -1,5 +1,27 @@
 package ru.Javabegin.FastJava.Shop3.goods;
 
-public class HardDrive extends ElectronicDevice{
+public class HardDrive extends ElectronicDevice {
+
+	private int volume;
+
+	public void format() {
+
+	}
+
+	public void copy() {
+
+	}
+
+	public void delete() {
+
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
 
 }
