9f9206e918e0b729150f41a71479c15b83e4497d
Ruslan <breitovo76@gmail.com>
2017-04-08 15:26:03 +0300

    add code in TVSet

--- Eclipe2/src/ru/Javabegin/FastJava/Shop3/goods/TVSet.java
+++ Eclipe2/src/ru/Javabegin/FastJava/Shop3/goods/TVSet.java
@@ -1,5 +1,19 @@
 package ru.Javabegin.FastJava.Shop3.goods;
 
-public class TVSet extends ElectronicDevice{
+public class TVSet extends ElectronicDevice{ 
+	
+	private String model;
+	
+	public void selectChannel( ){ 
+		
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
 
 }
