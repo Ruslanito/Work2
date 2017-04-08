f47dd1e032a0bfffb00c0be3e50a617e93179180
Ruslan <breitovo76@gmail.com>
2017-04-08 14:15:38 +0300

    add package Shop3

---
+++ Eclipe2/src/ru/Javabegin/FastJava/Shop3/bank/BaseBank.java
@@ -0,0 +1,14 @@
+package ru.Javabegin.FastJava.Shop3.bank;
+
+public class BaseBank { 
+	private String name;
+	private String creditDescription;
+	 
+	public void checkInfo(){
+		
+	} 
+	public void giveCreit(){
+		
+	}
+
+}
