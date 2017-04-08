2436db7d09741db4cfaa5ca19bd0ebdaebd920f2
Ruslan <breitovo76@gmail.com>
2017-04-08 15:39:06 +0300

    add code in cashier

--- Eclipe2/src/ru/Javabegin/FastJava/Shop3/service/Cashier.java
+++ Eclipe2/src/ru/Javabegin/FastJava/Shop3/service/Cashier.java
@@ -1,5 +1,12 @@
 package ru.Javabegin.FastJava.Shop3.service;
 
-public class Cashier extends BaseEmployee{
+public class Cashier extends BaseEmployee {
 
+	public void getMoney() {
+
+	}
+
+	public void giveBonusCard() {
+
+	}
 }
