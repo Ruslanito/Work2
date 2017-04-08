4a0cc5038530f2b0b153da19d6be5f712da5f31d
Ruslan <breitovo76@gmail.com>
2017-04-08 15:37:17 +0300

    add code in BaseEmployee

--- Eclipe2/src/ru/Javabegin/FastJava/Shop3/service/BaseEmployee.java
+++ Eclipe2/src/ru/Javabegin/FastJava/Shop3/service/BaseEmployee.java
@@ -1,5 +1,35 @@
 package ru.Javabegin.FastJava.Shop3.service;
 
+import ru.Javabegin.FastJava.Shop3.department.BaseDepartment;
+
 public class BaseEmployee {
+
+	private String name;
+	private BaseDepartment department;
+	private boolean free;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BaseDepartment getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(BaseDepartment department) {
+		this.department = department;
+	}
+
+	public boolean isFree() {
+		return free;
+	}
+
+	public void setFree(boolean free) {
+		this.free = free;
+	}
 
 }
