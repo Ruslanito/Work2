55fe051e961871b8fc7ca881630c508977e37a42
Ruslan <breitovo76@gmail.com>
2017-04-08 15:01:39 +0300

    add code in package client

--- Eclipe2/src/ru/Javabegin/FastJava/Shop3/client/VipVisitor.java
+++ Eclipe2/src/ru/Javabegin/FastJava/Shop3/client/VipVisitor.java
@@ -1,5 +1,21 @@
 package ru.Javabegin.FastJava.Shop3.client;
 
-public class VipVisitor extends BaseVisitor{
+public class VipVisitor extends BaseVisitor {
+	private float discount;
+
+	@Override
+	public void buyGoods() {
+		if (!checkDiscount()) {
+			super.buyGoods(); // купить со скидкой discount
+		} else {
+
+		}
+
+	}
+
+	private boolean checkDiscount() {
+		return discount > 0;
+
+	}
 
 }
