85f9277dcffc96ee971730b6de39b9eae4117106
Ruslan <breitovo76@gmail.com>
2017-04-08 15:30:07 +0300

    add code in Computer

--- Eclipe2/src/ru/Javabegin/FastJava/Shop3/goods/Computer.java
+++ Eclipe2/src/ru/Javabegin/FastJava/Shop3/goods/Computer.java
@@ -1,5 +1,19 @@
 package ru.Javabegin.FastJava.Shop3.goods;
 
-public class Computer extends ElectronicDevice {
+public class Computer extends ElectronicDevice { 
+	
+	private int ram;
+	
+	public void loadOS( ){
+		
+	}
+
+	public int getRam() {
+		return ram;
+	}
+
+	public void setRam(int ram) {
+		this.ram = ram;
+	}
 
 }
