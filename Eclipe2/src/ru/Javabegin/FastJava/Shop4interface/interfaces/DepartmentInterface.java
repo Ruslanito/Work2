2666386a51e0432f6ae5ca568127712f5e4b4243
Ruslan <breitovo76@gmail.com>
2017-04-10 16:32:27 +0300

    add DepartmentInterface

--- Eclipe2/src/ru/Javabegin/FastJava/Shop4interface/interfaces/DepartmentInterface.java
+++ Eclipe2/src/ru/Javabegin/FastJava/Shop4interface/interfaces/DepartmentInterface.java
@@ -1,5 +1,13 @@
 package ru.Javabegin.FastJava.Shop4interface.interfaces;
 
+import java.util.ArrayList;
+
 public interface DepartmentInterface {
+
+	String getName();
+
+	ArrayList<BaseEmployee> getEmployeeList();
+
+	ArrayList<BaseGoods> getGoodsList();
 
 }
