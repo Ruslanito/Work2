f8ddecccc3f957b4dd84bcaa5df165cedc140e3e
Ruslan <breitovo76@gmail.com>
2017-04-11 15:14:49 +0300

    add code in goods package

--- Eclipe2/src/ru/Javabegin/FastJava/Shop4interface/goods/HardDrive.java
+++ Eclipe2/src/ru/Javabegin/FastJava/Shop4interface/goods/HardDrive.java
@@ -1,5 +1,27 @@
 package ru.Javabegin.FastJava.Shop4interface.goods;
 
-public class HardDrive {
+public class HardDrive extends ElectronicDevice {
+	private int volume;
+
+	public void format() {
+
+	}
+
+	public void copy() {
+
+	}
+
+	public void delete() {
+
+	}
+
+	@Override
+	public void on() {
+
+	} 
+	@Override 
+	public void off(){ 
+		
+	}
 
 }
