3749f34230ac40d9e719166f78a0712654e6d1b0
Ruslan_S <Ruslan_S@ruslan>
2017-05-15 16:58:38 +0300

    add new class

--- Eclipe2/src/com/ruslanito/Core/Core_MethodsAndClasses/TwoMethods_OneClass1.java
+++ Eclipe2/src/com/ruslanito/Core/Core_MethodsAndClasses/TwoMethods_OneClass1.java
@@ -1,13 +1,13 @@
 package com.ruslanito.Core.Core_MethodsAndClasses;
 
-public class TwoMethods_OneClass {
+public class TwoMethods_OneClass1 {
 	final static String txt = "Это глобальная переменная класса TwoMethodsOneClassSimple";
 	
 	public static void main(String []args){
 		String txt = "Это локальная переменная метода main";
 		System.out.println(txt);
 		sub();
-		System.out.println(TwoMethods_OneClass.txt);
+		System.out.println(TwoMethods_OneClass1.txt);
 	}
 	public static void sub() { 
 		String ttt = "Это локальная переменная метода sub";
